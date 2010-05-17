61159b4e4837c6b6a31c1b066a3e6bfd3da8c3d7
cweiske <cweiske@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-05-17 12:25:13 +0000

    import tagged version 0.5.0 from our svn
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/nr_easy_workspace/tags/Version-0-5-0@33235 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,47 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "nr_easy_workspace".
+#
+# Auto generated 07-05-2010 16:22
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Easy Workspace',
+	'description' => 'An easy workspace administration tool for editors and reviewers.',
+	'category' => 'be',
+	'author' => 'Netresearch',
+	'author_email' => 'info@netresearch.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.5.0',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '4.2.1-0.0.0',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"43cb";s:6:"README";s:4:"ad0f";s:36:"class.tx_nreasyworkspace_tcemain.php";s:4:"6cf2";s:12:"ext_icon.gif";s:4:"a459";s:17:"ext_localconf.php";s:4:"f76b";s:14:"doc/manual.pdf";s:4:"19c8";s:14:"doc/manual.sxw";s:4:"32c5";s:38:"mod1/class.ux_SC_mod_user_ws_index.php";s:4:"500a";s:27:"mod1/class.ux_wslib_gui.php";s:4:"e473";s:30:"mod1/class.ux_wsol_preview.php";s:4:"9195";s:18:"mod1/locallang.xml";s:4:"fa25";s:17:"templates/ws.html";s:4:"11ad";}',
+	'suggests' => array(
+	),
+);
+
+?>
---
+++ mod1/class.ux_SC_mod_user_ws_index.php
@@ -0,0 +1,419 @@
+<?php
+/**
+ * Netresearch easy workspace extension for Typo3
+ *
+ * PHP version 5
+ *
+ * @category Typo3
+ * @package  nr_easy_workspace
+ * @author   Alexander Opitz <alexander.opitz@netresearch.de>
+ * @license  http://www.gnu.org/licenses/gpl.txt GPL v3
+ * @link     http://www.netresearch.de
+ */
+if(!defined ('TYPO3_MODE')) {
+    die ('Access denied.');
+}
+
+require_once 'conf.php' ;
+require_once $BACK_PATH . 'mod/user/ws/index.php';
+require_once PATH_typo3 . 'sysext/lang/lang.php';
+
+$LANG = t3lib_div::makeInstance('language');
+$LANG->init($BE_USER->uc['lang']);
+
+$LANG->includeLLFile('EXT:nr_easy_workspace/mod1/locallang.xml');
+
+/**
+ * The class which extends normal workspace gui.
+ *
+ * @category Typo3
+ * @package  nr_easy_workspace
+ * @author   Alexander Opitz <alexander.opitz@netresearch.de>
+ * @license  http://www.gnu.org/licenses/gpl.txt GPL v3
+ * @link     http://www.netresearch.de
+ * @see      SC_mod_user_ws_index
+ */
+class ux_SC_mod_user_ws_index extends SC_mod_user_ws_index
+{
+    /**
+     * Executes action for selected elements, if any is sent:
+     *
+     * @return void
+     */
+    function execute()
+    {
+        $post = t3lib_div::_POST();
+        if (is_array($post) && isset($post['cmd'])) {
+            $tce = t3lib_div::makeInstance('t3lib_TCEmain');
+            $tce->stripslashes_values = 0;
+            $tce->start(array(), $post['cmd']);
+            $tce->process_cmdmap();
+            $this->sendEmails();
+        } else {
+            parent::execute();
+        }
+    }
+
+    /**
+     * Initialize output with JS an header
+     *
+     * @return void
+     */
+    function init()
+    {
+        global $TYPO3_CONF_VARS, $LANG;
+
+        parent::init();
+
+        $filename = 'EXT:nr_easy_workspace/templates/ws.html';
+        
+        // If you symlink your typo3, then php resolves this symlinks
+        // and so you can't change from typo3 to typo3conf via ../ path changes.
+        $strBackPathSave = $this->doc->backPath;
+        $this->doc->backPath = '';
+        //Typo3 before 4.3 don't handle extension path
+        $filename = t3lib_extMgm::extPath('nr_easy_workspace') .'templates/ws.html';
+        $this->doc->setModuleTemplate($filename);
+        $this->doc->backPath = $strBackPathSave;
+
+        $this->doc->JScode.= $this->doc->wrapScriptTags(
+            'arTexte = new Array("'
+            . sprintf($LANG->getLL('mailtext_1'), $TYPO3_CONF_VARS['SYS']['sitename'])
+            . '","'
+            . sprintf($LANG->getLL('mailtext_2'), $TYPO3_CONF_VARS['SYS']['sitename'])
+            . '","'
+            . sprintf($LANG->getLL('mailtext_3'), $TYPO3_CONF_VARS['SYS']['sitename'])
+            . '"
+                            );
+            function testSendButtons() {
+                arElements = document.getElementsByTagName("input");
+                bSomething = false;
+                for(var i = 0; i < arElements.length; i++) {
+                    if (arElements[i].type == "checkbox" && arElements[i].checked) {
+                        bSomething = true;
+                        break;
+                    }
+                }
+                if (bSomething) {
+                    setButton("submit_marked", true);
+                    reviewer = document.getElementById("reviewer");
+                    message_ind = document.getElementById("message_ind");
+                    if (reviewer.value!="" && message_ind.value!="") {
+                        setButton("submit_marked_to_review", true);
+                    } else {
+                        setButton("submit_marked_to_review", false);
+                    }
+                } else {
+                    setButton("submit_marked_to_review", false);
+                    setButton("submit_marked_to_review", false);
+                }
+            }
+
+            function setButton(name, enable) {
+                button = document.getElementById(name);
+                if (button) {
+                    if (enable) {
+                        button.disabled = false;
+                        button.style.opacity = 1;
+                    } else {
+                        button.disabled = true;
+                        button.style.opacity = 0.3;
+                    }
+                }
+            }
+
+            function updateText() {
+                message = document.getElementById("message");
+                message_ind = document.getElementById("message_ind");
+                if (message.value != "") {
+                    message_ind.value = arTexte[message.value];
+                }
+            }'
+        );
+    }
+
+    /**
+     * Main output function
+     *
+     * @return void
+     */
+    function main()
+    {
+        global $LANG, $BE_USER, $BACK_PATH;
+
+        // See if we need to switch workspace
+        $changeWorkspace = t3lib_div::_GET('changeWorkspace');
+        if ($changeWorkspace != '') {
+            $BE_USER->setWorkspace($changeWorkspace);
+            $this->content .= $this->doc->wrapScriptTags(
+                'top.location.href="' . $BACK_PATH . t3lib_BEfunc::getBackendScript() . '";'
+            );
+        } else {
+            // Starting page:
+            $this->content .= $this->doc->header($LANG->getLL('title'));
+            $this->content .= $this->doc->spacer(5);
+
+            // Get usernames and groupnames
+            $be_group_Array = t3lib_BEfunc::getListGroupNames('title,uid');
+            $groupArray = array_keys($be_group_Array);
+
+            // Need 'admin' field for t3lib_iconWorks::getIconImage()
+            $this->be_user_Array_full = $this->be_user_Array
+                = t3lib_BEfunc::getUserNames(
+                    'username,usergroup,usergroup_cached_list,uid,admin'
+                    . ',workspace_perms,email,realName'
+                );
+            if (!$BE_USER->isAdmin()) {
+                $this->be_user_Array = t3lib_BEfunc::blindUserNames(
+                    $this->be_user_Array, $groupArray, 1
+                );
+            }
+
+            if ($BE_USER->getTSConfigVal('workspace_showall') == 1) {
+                    // Build top menu:
+                $menuItems = array();
+                $menuItems[] = array(
+                    'label' => $LANG->getLL('menuitem_review'),
+                    'content' => $this->moduleContent_publish()
+                );
+                $menuItems[] = array(
+                    'label' => $LANG->getLL('menuitem_workspaces'),
+                    'content' => $this->moduleContent_workspaceList()
+                );
+
+                    // Add hidden fields and create tabs:
+                $content = $this->doc->getDynTabMenu($menuItems,'user_ws');
+            } else {
+                if ($BE_USER->workspace != 0) {
+                    // if workspace is not live
+                    $content = $this->displayWorkspaceOverview();
+                } else {
+                    $content = $this->doc->section(
+                        $LANG->getLL('live_not_useable'),
+                        '', true, false, 3
+                    );
+                }
+                $strWorkspace = (
+                    $BE_USER->workspace == 0
+                    ? 'LIVE'
+                    : $BE_USER->workspaceRec['title']
+                );
+                $content = $this->doc->section(
+                    $LANG->getLL('label_workspace') . ' ' . $strWorkspace,
+                    $content, 1, 1
+                );
+            }
+            $this->content.=$this->doc->section('', $content, 0, 1);
+        }
+            // Setting up the buttons and markers for docheader
+        $docHeaderButtons = $this->getButtons();
+        $markers['CONTENT'] = $this->content;
+
+            // Build the <body> for the module
+        $this->content = $this->doc->startPage($LANG->getLL('title'));
+        $this->content.= $this->doc->moduleBody(
+            $this->pageinfo, $docHeaderButtons, $markers
+        );
+        $this->content.= $this->doc->endPage();
+
+        $this->content = $this->doc->insertStylesAndJS($this->content);
+    }
+
+    /**
+     * Depending on TSconfig, it returns the Workspace
+     *
+     * @return string HTML rendered output
+     */
+    function displayWorkspaceOverview()
+    {
+        global $BE_USER;
+        $content = parent::displayWorkspaceOverview();
+        if ($BE_USER->getTSConfigVal('workspace_showall') != 1) {
+            $content.= $this->getReviewersOverviewEasy();
+        }
+        return $content;
+    }
+
+    /**
+     * Gets list of available reviewers for this workspace
+     *
+     * @return array Array of BE_Users which are reviewer
+     */
+    function getReviewers()
+    {
+        global $BE_USER;
+        $list = $BE_USER->workspaceRec['reviewers'];
+        $content_array = array();
+        if ($list != '') {
+            $userIDs = explode(',', $list);
+
+                // get user names and sort
+            $regExp = '/^(be_[^_]+)_(\d+)$/';
+            $groups = false;
+            foreach ($userIDs as $userUID) {
+                $id = $userUID;
+
+                if (preg_match($regExp, $userUID)) {
+                    $table = preg_replace($regExp, '\1', $userUID);
+                    $id = intval(preg_replace($regExp, '\2', $userUID));
+                    if ($table == 'be_users') {
+                        // user
+                        $this->getUserIntoArray($id, $content_array);
+                    } else {
+                        // group
+                        if (false === $groups) {
+                            $groups = t3lib_BEfunc::getGroupNames();
+                        }
+                        foreach ($this->be_user_Array_full as $user) {
+                            $arGroups = split(',', $user['usergroup_cached_list']);
+                            if (in_array($id, $arGroups)) {
+                                $this->getUserIntoArray(
+                                    $user['uid'], $content_array
+                                );
+                            }
+                        }
+                    }
+                } else {
+                    $this->getUserIntoArray($userUID, $content_array);
+                }
+            }
+            asort($content_array);
+        }
+        return $content_array;
+    }
+
+    /**
+     * Puts the user with the id into the array with realname or username, if
+     * an email is defined for the user.
+     *
+     * @param integer $id       The user id.
+     * @param array   &$arUsers The array with users.
+     *
+     * @return void
+     */
+    function getUserIntoArray($id, &$arUsers)
+    {
+        if (isset($this->be_user_Array_full[$id]['email'])) {
+            $arUsers[$id] = (
+                $this->be_user_Array_full[$id]['realName'] != ''
+                ? $this->be_user_Array_full[$id]['realName']
+                : $this->be_user_Array_full[$id]['username']
+            );
+        }
+    }
+
+    /**
+     * Builds the reviewer part of the workspace overview.
+     *
+     * @return string HTML generated from template
+     */
+    function getReviewersOverviewEasy()
+    {
+        global $LANG, $BE_USER;
+        // @NETRESEARCH Die Mitteilungsbox an die Reviewer
+        $arMessages = array(
+            $LANG->getLL('mailtext_1_header'),
+            $LANG->getLL('mailtext_2_header'),
+            $LANG->getLL('mailtext_3_header')
+        );
+        $arReviewers = $this->getReviewers();
+        $strRevOption = '<option value="">'
+            . $LANG->getLL('select_reviewer')
+            . '</option>';
+        $strMsgOption = '<option value="">'
+            . $LANG->getLL('select_message')
+            . '</option>';
+
+        $strTable = t3lib_parsehtml::getSubpart(
+            $this->doc->moduleTemplate,
+            '###WORKSPACE_REVIEWER_TABLE###'
+        );
+
+        $arLLL = array(
+            '###LLL_INFORM_REVIEWER###'      => $LANG->getLL('inform_reviewer'),
+            '###LLL_REVIEWER###'             => $LANG->getLL('reviewer'),
+            '###LLL_REVIEWER_MESSAGE###'     => $LANG->getLL('reviewer_message'),
+            '###LLL_REVIEWER_MESSAGE_IND###' => $LANG->getLL('reviewer_message_ind'),
+            '###LLL_SUBMIT_MARKED_TO_REVIEW###'
+                => $LANG->getLL('submit_marked_to_review'),
+            '###LLL_NO_REVIEWERS###'         => $LANG->getLL('no_reviewers'),
+        );
+
+        $strTable = t3lib_parsehtml::substituteMarkerArray(
+            $strTable,
+            $arLLL
+        );
+
+        if (count($arReviewers)) {
+            foreach ($arReviewers as $uid => $name) {
+                $strRevOption.= '<option value="'.$uid.'">'.$name.'</option>';
+            }
+            foreach ($arMessages as $uid => $name) {
+                $strMsgOption.= '<option value="'.$uid.'">'.$name.'</option>';
+            }
+            $arData = array(
+                '###OPTIONS_REVIEWER###' => $strRevOption,
+                '###OPTIONS_MESSAGES###' => $strMsgOption,
+            );
+
+            $strTable = t3lib_parsehtml::substituteMarkerArray(
+                $strTable,
+                $arData
+            );
+
+            $strTable = t3lib_parsehtml::substituteSubpart(
+                $strTable,
+                '###WORKSPACE_NO_REVIEWER_AVAILABLE###',
+                ''
+            );
+        } else {
+            $strTable = t3lib_parsehtml::substituteSubpart(
+                $strTable,
+                '###WORKSPACE_REVIEWER_AVAILABLE###',
+                ''
+            );
+        }
+
+        return $strTable;
+    }
+
+    /**
+     * Sends out the reviewer mails if set in TYPO3_CONF_VARS
+     *
+     * @return void
+     */
+    function sendEmails()
+    {
+        global $TYPO3_CONF_VARS, $BE_USER, $LANG;
+        $arSendMails
+            = $TYPO3_CONF_VARS['SC_OPTIONS']['tx_nreasyworkspace_tcemain']['sendMails'];
+
+        if (isset($arSendMails['to']) && $arSendMails['to'] != '') {
+            $emails = $arSendMails['to'];
+            $message = sprintf($arSendMails['message'], $arSendMails['toreplace']);
+            $message .= "\n" . $LANG->getLL('message_notice');
+            $message .= "\n\n" . $BE_USER->user['realName'];
+            $message .= "\n" .  (
+                $BE_USER->user['email'] != ''
+                ? $BE_USER->user['email'] : ''
+            );
+
+            $returnPath = (
+                $BE_USER->user['email'] != ''
+                ? $BE_USER->user['realName'] . ' <' . $BE_USER->user['email'] . '>'
+                : (
+                    $TYPO3_CONF_VARS['SYS']['siteemail'] != ''
+                    ? $TYPO3_CONF_VARS['SYS']['siteemail'] :
+                    'YourTypo3@installation.org'
+                )
+            );
+
+            t3lib_div::plainMailEncoded(
+                implode(',',$emails),
+                $arSendMails['title'],
+                trim($message),
+                'From: '.$returnPath."\n"
+            );
+        }
+    }
+}
